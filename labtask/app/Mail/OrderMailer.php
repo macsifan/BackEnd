c7d4f13c293f3585c0122aaf5a75943168861920
macsifan <redniger99@gmail.com>
2019-10-16 23:41:33 +0600

    lab

---
+++ labtask/app/Mail/OrderMailer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderMailer extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.orders')
+
+        ->with([
+        
+        'orderName' => $this->order->name,
+        
+        'orderPrice' => $this->order->price,
+        
+        ]);
+    }
+}
